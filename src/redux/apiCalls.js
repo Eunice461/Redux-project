c3172e98e514d028e6fe046e7ec4f3d3eeccca46
Akaniru Eunice <chieunice114@gmail.com>
2022-01-10 01:31:15 -0800

    integrate a mini api using redux

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { updateStart, updateSuccess, updateError } from "./userSlice";
 
-export const updateUser = async (user, dispatch) {
+export const updateUser = async (user, dispatch) => {
     dispatch(updateStart());
     try {
         const res = await axios.post("http:localhost:8800//api/user/:id/update")
